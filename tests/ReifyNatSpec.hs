9976c667f8f2d58b1db02317fc9a90fc63314057
Ryan Scott <ryan.gl.scott@gmail.com>
2023-01-21 22:11:44 -0500

    Work around mysterious, GHC 9.0–only test failure

--- tests/ReifyNatSpec.hs
+++ tests/ReifyNatSpec.hs
@@ -3,14 +3,18 @@
 module ReifyNatSpec where
 
 #if __GLASGOW_HASKELL__ >= 708
-import Control.Exception (ArithException(..), evaluate)
 import Data.Reflection
 import Test.Hspec.QuickCheck
-import Test.QuickCheck (Negative(..), NonNegative(..))
+import Test.QuickCheck (NonNegative(..))
 
 # if MIN_VERSION_base(4,10,0)
 import GHC.TypeNats (natVal)
 import Numeric.Natural (Natural)
+# endif
+
+# if __GLASGOW_HASKELL__ != 900
+import Control.Exception (ArithException(..), evaluate)
+import Test.QuickCheck (Negative(..))
 # endif
 #endif
 
@@ -25,9 +29,16 @@ spec =
   describe "reifyNat" $ do
     prop "reify positive Integers and reflect them back" $
       \(NonNegative (i :: Integer)) -> reifyNat i $ \p -> reflect p `shouldBe` i
+# if __GLASGOW_HASKELL__ != 900
+    -- Inexplicably, this test fails on GHC 9.0 with hspec-2.8.4 or later.
+    -- Moreover, I suspect that undefined behavior is involved in some way,
+    -- as the output of hspec will occasionally be swallowed entirely. I have
+    -- not been able to observe this phenomenon on any other version of GHC,
+    -- so I will simply disable this test on 9.0.
     prop "should throw an Underflow exception on negative inputs" $
       \(Negative (i :: Integer)) ->
         reifyNat i (evaluate . reflect) `shouldThrow` (== Underflow)
+# endif
 # if MIN_VERSION_base(4,10,0)
     it "should reflect very large Naturals correctly" $ do -- #41
       let d42, d2_63, d2_64 :: Natural
